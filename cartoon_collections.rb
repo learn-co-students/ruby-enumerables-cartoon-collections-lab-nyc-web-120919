61533eae231f4d9a17379540ce15bb975490d10a
Calvin Baker <calvin.baker128@gmail.com>
2019-12-08 06:32:05 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,31 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(names)# code an argument here
   # Your code here
+  new_names = names.each_with_index { | x, i | 
+    puts "/#{i + 1}.*#{x}/"
+  }
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_plane(calls)t# code an argument here
   # Your code here
+  fixed_calls = calls.map{ |x| 
+    "#{x[0].upcase}#{x[1..-1]}!"
+  }
+  puts fixed_calls
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(words)# code an argument here
   # Your code here
+  words.each{ |a| 
+    if a.length > 4
+      return true
+    end
+  }
+  return false
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(snacks)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  snacks.each{ | x | 
+    if 
 end
